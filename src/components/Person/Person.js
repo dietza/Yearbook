c2ee2002f28f408c3c51089ab52c4ae2c2da0792
Allison Dietz <dietz.allison@outlook.com>
2021-02-02 01:54:44 -0700

    Add bones of Person component

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import './Person.css';
 
-// PERSON COMPONENT CODE GOES HERE
+const Person = () => {
+  return (
+    <div className="person-card"> 
+
+    </div>
+  )
+}
 
 export default Person;
