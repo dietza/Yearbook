997aee6726785f521bbba45035123fc951c8e283
Allison Dietz <dietz.allison@outlook.com>
2021-02-02 02:23:07 -0700

    Build card info in Person component

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './Person.css';
 
-const Person = () => {
+const Person = (props) => {
   return (
     <div className="person-card"> 
 
+      <img src={ props.image } style={{maxHeight:"15rem", maxWidth:"15rem"}} alt="bird"/>
+      <h2>{ props.name }</h2>
+      <h3>{ props.quote }</h3>
+      <p>{ props.superlative }</p>
     </div>
   )
 }
