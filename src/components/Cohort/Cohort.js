ee5d5a79bdb9bee84778de29722b92a2b14e215d
Allison Dietz <dietz.allison@outlook.com>
2021-02-02 02:11:44 -0700

    Map staff data to display in Cohort

--- src/components/Cohort/Cohort.js
+++ src/components/Cohort/Cohort.js
@@ -3,11 +3,23 @@ import Person from '../Person/Person';
 import './Cohort.css';
 
 const Cohort = ({ staff }) => {
+
+  const profilesOnDisplay = staff.map(person => {
+    
+    // return <Person person={ person }/>
+    
+    return <Person
+      image={ person.photo }
+      name={ person.name }
+      quote={ person.quote }
+      superlative={ person.superlative }
+      key={ person.id }
+      />
+  })
+
   return (
     <div className="cohort-container">
-
-
-
+      { profilesOnDisplay }
     </div>
   )
 }
