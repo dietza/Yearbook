a4797a62dd5df767ec76320534de545e864dcd9b
Allison Dietz <dietz.allison@outlook.com>
2021-02-02 02:34:48 -0700

    Resize bird images

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -4,8 +4,7 @@ import './Person.css';
 const Person = (props) => {
   return (
     <div className="person-card"> 
-
-      <img src={ props.image } style={{maxHeight:"15rem", maxWidth:"15rem"}} alt="bird"/>
+      <img src={ props.image } alt="cool bird"/>
       <h2>{ props.name }</h2>
       <h3>{ props.quote }</h3>
       <p>{ props.superlative }</p>
